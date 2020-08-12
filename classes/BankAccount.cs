1f1d8dfeb6961b9d1dd2985208a855a271c2fc83
Felix Wang <felix_wzj@yahoo.com>
2020-08-12 09:09:44 +0800

    Add files via upload

---
+++ classes/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            // this.Balance = initialBalance;
+            //var initialTransaction = new Transaction(initialBalance, DateTime.Now , "Initial Transaction");
+            //allTransactions.Add(initialTransaction);
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance for " + name);
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit mush be positive.");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal mush be positive.");
+            }
+            if(this.Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Now sufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\tTransaction\tBalance\tNote");
+            foreach(var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Felix Wang", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(100, DateTime.Now, "Salary");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("Mr. INVALID", -100);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.Beep();
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                account.MakeWithdrawal(1000, DateTime.Now, "Invalid Withdraw");
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.Beep();
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
